32d929f1583fe2b9dafa44d892bb42e4a702b020
KrishnaK2507 <krishnakale52@gmail.com>
2024-05-17 02:02:09 +0530

    First

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,26 @@
 const cl = console.log;
+const stdContainer = document.getElementById("stdContainer");
+const stdForm = document.getElementById("stdForm");
+const fname = document.getElementById("fname");
 
-const todoForm = document.getElementById("todoForm");
-const todoList = document.getElementById("todoList");
-const todoItemControl = document.getElementById("todoItem");
 
-const todoArr =[
+
+const stdArr = [
     {
-        todoItem : "Javascript",
-        todoid : 1234
-    }
-];
+        fname : "John",
+        lname : "Doe",
+        email : "johndoe56@gmail.com",
+        contact : 456123,
+        id : 12345
+    },
+    {
+        fname : "Jen",
+        lname : "Demark",
+        email : "jendenm56@gmail.com",
+        contact : 789426,
+        id : 2561
+    },
+]
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
@@ -18,34 +29,43 @@ function uuidv4() {
             v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
+}
+
+const createStd = (arr, idContainer) => {
+    let result = ``;
+  arr.forEach((std, i )=>{
+  result += `<tr id="${std.id}">
+  <td>${i+1}</td>
+  <td>${std.fname}</td>
+  <td>${std.lname}</td>
+  <td>${std.email}</td>
+  <td>${std.contact}</td>
+</tr>`
+})
+idContainer.innerHTML = result;
 };
 
-
-const creatTodoList = (arr) =>{
-    let result ="";
-    arr.forEach(todo => {
-        result += `<li class="list-group-item" id="${todo.todoid}">${todo.todoItem}</li>`
-    });
-
-    todoList.innerHTML = result;
-}
-
-creatTodoList(todoArr)
+createStd(stdArr, stdContainer);
 
 
-const ontodoAdd = (eve) =>{
-    eve.preventDefault();
-    let newTodo = {
-        todoItem : todoItemControl.value,
-        todoid : uuidv4()
-    }
+// idContainer.innerHTML = result;
 
-    todoArr.unshift(newTodo)
-    cl(todoArr);
-    creatTodoList(todoArr);
-    todoForm.reset();
+
+const onStdAdd = (eve) => {
+ eve.preventDefault();
+ let newStd = {
+    fname : eve.target['0'].value,
+    lname : eve.target['1'].value,
+    email : eve.target['2'].value,
+    contact : eve.target['3'].value,
+    id : uuidv4()
+ }
+
+ stdArr.unshift(newStd);
+ createStd(stdArr, stdContainer);
+ eve.target.reset();
 }
 
 
 
-todoForm.addEventListener("submit", ontodoAdd);
+stdForm.addEventListener("submit", onStdAdd);
